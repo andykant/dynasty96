6e22327abc03b7d2d0eaba01de83365a676990aa
Andy Kant <andy@andykant.com>
2016-02-28 02:16:04 -0600

    Added pick status support

--- src/client/views/franchise.js
+++ src/client/views/franchise.js
@@ -5,6 +5,10 @@ import { Franchise, League, DraftResults, Players } from "../stores";
 import Team from "./team";
 import Player from "./player";
 
+function overall(pick) {
+	return (parseInt(pick.round,10) - 1) * 96 + parseInt(pick.pick,10);
+};
+
 export default React.createClass({
 	mixins: [
 		Reflux.connect(Franchise, "franchise"),
@@ -21,10 +25,20 @@ export default React.createClass({
 
 	renderFranchise: function() {
 		var franchise = this.state.franchise;
+		var draftResults = this.state.draftResults.filter((pick) => pick.franchise === franchise.id);
+		var currentPick = this.state.draftResults.find((pick) => pick.timestamp === 0);
+		var nextPick = this.state.draftResults.find((pick) => pick.timestamp === 0 && pick.franchise === franchise.id);
+		var difference = currentPick && nextPick ? (overall(nextPick) - overall(currentPick)) : null;
 		return <div className="franchise-roster">
 			<Team {...franchise} onClick={Actions.clearFranchise} />
-			{this.state.draftResults && this.state.draftResults
-				.filter((pick) => franchise && pick.player && pick.franchise === franchise.id)
+			{difference !== null && <div className="franchise-next">
+				{difference === 0
+					? <span className="franchise-next-draft">It's my turn to pick #{parseInt(nextPick.round,10) + "." + nextPick.pick}!</span>
+					: <span><span className="franchise-next-pick">{difference}</span> pick{difference !== 1 ? "s" : ""} until my next pick <span className="franchise-next-pick">#{parseInt(nextPick.round,10) + "." + nextPick.pick}</span></span>
+				}
+			</div>}
+			{draftResults && draftResults
+				.filter((pick) => pick.player)
 				.map((pick, i) => {
 					var player = Players.byId(pick.player);
 					return <Player key={player ? player.id : i} {...player} compact={true} />;
